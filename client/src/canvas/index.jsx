8de5e6f2461d0dde3321d1be6f28380905fcba4a
OmarSerranoSosa <omar.serranososa@gmail.com>
2023-07-18 17:50:17 -0600

    first commit

---
+++ client/src/canvas/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Canvas = () => {
+  return (
+    <div>
+       <h2>canvas</h2>
+    </div>
+  )
+}
+
+export default Canvas
---
+++ client/src/pages/Customizer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Customizer = () => {
+  return (
+    <div>
+      <h2>Customizer</h2>
+    </div>
+  )
+}
+
+export default Customizer
