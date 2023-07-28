373548df89ec66497f490badd969aed0199d6702
OmarSerranoSosa <omar.serranososa@gmail.com>
2023-07-27 20:41:41 -0600

    few things left to do

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -95,7 +95,7 @@ const Customizer = () => {
         break
       case "stylishShirt":
         state.isFullTexture = !activeFilterTab[tabName]
-
+        break
       default:
         state.isLogoTexture = true
         state.isFullTexture = false
